0565ceab238caf15e6ab4d563e766bca6f3759ad
edenlisk <nkumbuyenock@gmail.com>
2023-03-02 12:02:25 +0200

    add table and use joins

--- data.sql
+++ data.sql
@@ -14,3 +14,42 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
 ('Blossmon', '1998-10-13', 3, TRUE, 17),
 ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+---------------------------------------------------------------------
+-- INSERT SAMPLE DATA INSIDE OWNERS TABLE.
+INSERT INTO owners (full_name, age) VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+-- INSERT SAMPLE DATA INSIDE SPECIES TABLE.
+INSERT INTO species (name) VALUES
+('Pokemon'),
+('Digimon');
+
+
+UPDATE animals
+SET species_id = 2
+WHERE animals.name LIKE '%mon';
+
+
+UPDATE animals
+SET species_id = 1
+WHERE animals.name NOT LIKE '%mon';
+SELECT * FROM animals;
+
+-- updating animals with corresponding owners
+UPDATE animals
+SET owners_id= 1 WHERE name ='Agumon';
+UPDATE animals
+SET owners_id= 2 WHERE name IN ('Gabumon','Pikachu');
+UPDATE animals
+SET owners_id= 3 WHERE name IN ('Devimon','Plantmon');
+UPDATE animals
+SET owners_id= 4 WHERE name IN ('Charmander','Squirtle','Blossom');
+UPDATE animals
+SET owners_id= 5 WHERE name IN ('Angemon','Boarmon');
+SELECT * FROM animals;
--- schema.sql
+++ schema.sql
@@ -11,3 +11,45 @@ weight_kg DECIMAL
 
 ALTER TABLE animals
 ADD species VARCHAR(50);
+
+
+---------------------------------------------------------------------
+
+-- Create owners table
+CREATE TABLE owners (
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+full_name VARCHAR(50),
+age INT);
+
+
+-- Create species table
+CREATE TABLE species (
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+name VARCHAR(50));
+
+-- DROP SPECIES COLUMN FROM animals table
+ALTER animals
+DROP COLUMN species;
+
+
+-- Create species_id column
+ALTER TABLE animals
+ADD species_id INT;
+
+-- Create owners_id column
+ALTER TABLE animals
+ADD owners_id INT;
+
+-- Add foreign key constraint on species_id referencing species table
+ALTER TABLE animals
+ADD CONSTRAINT species_fk
+FOREIGN KEY(species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+-- Add foreign key constraint on owners_id referencing owners table
+ALTER TABLE animals
+ADD CONSTRAINT owners_fk
+FOREIGN KEY(owners_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
